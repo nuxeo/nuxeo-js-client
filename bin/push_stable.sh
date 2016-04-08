491938c8844499e663adc9ec6880d5f28eac3729
Thomas Roger <throger@gmail.com>
2016-04-08 15:51:01 +0200

    NXJS-64: update push_stable.sh script

--- bin/push_stable.sh
+++ bin/push_stable.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -
 set -e
 
+rm -rf /tmp/push_stable/
+mkdir -p /tmp/push_stable/
+
+cp -r lib /tmp/push_stable/
+cp -r dist /tmp/push_stable/
+
 git checkout -f stable
 git merge origin/master -s recursive -X theirs --no-edit
+
+mv -f /tmp/push_stable/* .
+
 git add -f dist lib
-git commit -m "Update stable branch\n $JOB_NAME#$BUILD_NUMBER"
+git commit -m $'Update stable branch\n $JOB_NAME#$BUILD_NUMBER'
+
+rm -rf /tmp/push_stable/
+
 git push origin stable:stable
