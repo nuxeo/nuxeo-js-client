4e042a31f8b4b0d456d8c5b51c735a5990e91922
Thomas Roger <throger@gmail.com>
2017-03-01 15:47:10 +0100

    Release 3.0.0

---
+++ dist/lib/auth/auth.js
@@ -0,0 +1,66 @@
+const md5 = require('md5');
+const Random = require('random-js');
+
+const { btoa } = require('../deps/utils/base64');
+
+const authenticators = {};
+
+const Authentication = {
+  registerAuthenticator: (method, authenticator) => {
+    authenticators[method] = authenticator;
+  },
+
+  computeAuthentication: (auth, headers) => {
+    if (auth) {
+      const authenticator = authenticators[auth.method];
+      if (authenticator) {
+        authenticator(auth, headers);
+      }
+    }
+    return headers;
+  },
+};
+
+// default authenticators
+const basicAuthenticator = (auth, headers) => {
+  if (auth.username && auth.password) {
+    const base64 = btoa(`${auth.username}:${auth.password}`);
+    const authorization = `Basic ${base64}`;
+    headers.Authorization = authorization;
+  }
+};
+
+const tokenAuthenticator = (auth, headers) => {
+  if (auth.token) {
+    headers['X-Authentication-Token'] = auth.token;
+  }
+};
+
+const bearerTokenAuthenticator = (auth, headers) => {
+  if (auth.token) {
+    headers.Authorization = `Bearer ${auth.token}`;
+  }
+};
+
+const random = Random.engines.mt19937().autoSeed();
+
+const portalAuthenticator = (auth, headers) => {
+  if (auth.secret && auth.username) {
+    const date = new Date();
+    const randomData = random();
+
+    const clearToken = [date.getTime(), randomData, auth.secret, auth.username].join(':');
+    const base64hashedToken = btoa(md5(clearToken, { asBytes: true }));
+    headers.NX_RD = randomData;
+    headers.NX_TS = date.getTime();
+    headers.NX_TOKEN = base64hashedToken;
+    headers.NX_USER = auth.username;
+  }
+};
+
+Authentication.basicAuthenticator = basicAuthenticator;
+Authentication.tokenAuthenticator = tokenAuthenticator;
+Authentication.bearerTokenAuthenticator = bearerTokenAuthenticator;
+Authentication.portalAuthenticator = portalAuthenticator;
+
+module.exports = Authentication;
---
+++ dist/lib/unmarshallers/unmarshallers.js
@@ -0,0 +1,63 @@
+const Document = require('../document');
+const Workflow = require('../workflow/workflow');
+const Task = require('../workflow/task');
+const User = require('../user/user');
+const Group = require('../group/group');
+
+const unmarshallers = {};
+
+const Unmarshallers = {
+  registerUnmarshaller: (entityType, unmarshaller) => {
+    unmarshallers[entityType] = unmarshaller;
+  },
+
+  unmarshall: (contentType, json, options) => {
+    const entityType = (contentType.parameters && contentType.parameters['nuxeo-entity']) || json['entity-type'];
+    const unmarshaller = unmarshallers[entityType];
+    return (unmarshaller && unmarshaller(json, options)) || json;
+  },
+};
+
+// default unmarshallers
+
+const documentUnmarshaller = (json, options) => new Document(json, options);
+
+const documentsUnmarshaller = (json, options) => {
+  const { entries } = json;
+  const docs = entries.map((doc) => new Document(doc, options));
+  json.entries = docs;
+  return json;
+};
+
+const workflowUnmarshaller = (json, options) => new Workflow(json, options);
+
+const workflowsUnmarshaller = (json, options) => {
+  const { entries } = json;
+  const workflows = entries.map((workflow) => new Workflow(workflow, options));
+  json.entries = workflows;
+  return json;
+};
+
+const taskUnmarshaller = (json, options) => new Task(json, options);
+
+const tasksUnmarshaller = (json, options) => {
+  const { entries } = json;
+  const tasks = entries.map((task) => new Task(task, options));
+  json.entries = tasks;
+  return json;
+};
+
+const userUnmarshaller = (json, options) => new User(json, options);
+
+const groupUnmarshaller = (json, options) => new Group(json, options);
+
+Unmarshallers.documentUnmarshaller = documentUnmarshaller;
+Unmarshallers.documentsUnmarshaller = documentsUnmarshaller;
+Unmarshallers.workflowUnmarshaller = workflowUnmarshaller;
+Unmarshallers.workflowsUnmarshaller = workflowsUnmarshaller;
+Unmarshallers.taskUnmarshaller = taskUnmarshaller;
+Unmarshallers.tasksUnmarshaller = tasksUnmarshaller;
+Unmarshallers.userUnmarshaller = userUnmarshaller;
+Unmarshallers.groupUnmarshaller = groupUnmarshaller;
+
+module.exports = Unmarshallers;
