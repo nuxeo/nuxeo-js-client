61932ee21fc5c3a71d2571b4276a932469fb8279
Thomas Roger <troger@nuxeo.com>
2018-09-11 16:55:12 +0200

    NXJS-163: remove Node.js 4 tests and support

--- test/helpers/setup-node-es5.js
+++
@@ -1,9 +0,0 @@
-/* eslint import/no-unresolved: 0 */
-const chai = require('chai');
-
-const Nuxeo = require('../../dist/es5');
-
-
-global.Nuxeo = Nuxeo;
-global.expect = chai.expect;
-global.isBrowser = false;
