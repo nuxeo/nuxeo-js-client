d8b3db1ffd9bc67656e7eb2e2bce37d0844e42a6
Thomas Roger <troger@nuxeo.com>
2023-03-20 14:25:05 +0100

    NXJS-212: put back old join function behavior

--- test/deps/utils/join.spec.js
+++ test/deps/utils/join.spec.js
@@ -1,12 +1,12 @@
 const join = require('../../../lib/deps/utils/join');
 
 describe('join', () => {
-  it('should deduplicate / between segments only', () => {
+  it('should deduplicate / except for links', () => {
     expect(join('http://localhost:8080/nuxeo/', '/api/v1/', '/directory/nature'))
       .to.be.eql('http://localhost:8080/nuxeo/api/v1/directory/nature');
     expect(join('/directory/nature', 'http://foo.com'))
       .to.be.eql('/directory/nature/http://foo.com');
     expect(join('////foo/bar//', '/foobar///'))
-      .to.be.eql('////foo/bar/foobar///');
+      .to.be.eql('/foo/bar/foobar/');
   });
 });
