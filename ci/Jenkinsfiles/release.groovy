49cf945402608d3af893568d9fb1d309bfd54340
Kevin Leturc <kevin.leturc@hyland.com>
2025-03-17 16:22:13 +0100

    NXBT-3857: Update shared library

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -19,7 +19,7 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-library identifier: "platform-ci-shared-library@v0.0.45"
+library identifier: "platform-ci-shared-library@v0.0.49"
 
 pipeline {
   agent {
