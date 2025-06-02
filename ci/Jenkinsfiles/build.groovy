0e8f8e52b0e12f7e39a5f8352b41c70b3e5a3f22
Kevin Leturc <kevin.leturc@hyland.com>
2025-06-02 12:46:58 +0200

    NXBT-3864: Update shared library

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -16,7 +16,7 @@
  * Contributors:
  *     Kevin Leturc <kevin.leturc@hyland.com>
  */
-library identifier: "platform-ci-shared-library@v0.0.55"
+library identifier: "platform-ci-shared-library@v0.0.67"
 
 String getNodeJsVersion(String containerId) {
   container(containerId) {
